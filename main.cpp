672e2aa88c9b5db6d5f31fd0d00773b114d6d1e8
Pedro V. M. Sampaio <105935494+pedrovxzs@users.noreply.github.com>
2025-01-22 08:35:56 -0300

    Analise de bugs da função insert

--- main.cpp
+++ main.cpp
@@ -10,12 +10,17 @@ int main()
     matriz.insert(1,2, 4);
     matriz.insert(1,1, 2);
     cout << matriz.get(1,2) << endl;
+    cout << matriz.get(1,1) << endl << endl;
+    matriz.print();
+    cout << endl;
+
+    // Testando a inserção de um valor na mesma coluna
+    matriz.insert(1,2, 8);
+    matriz.insert(1,1, 6);
     cout << matriz.get(1,2) << endl;
     cout << matriz.get(1,1) << endl << endl;
     matriz.print();
-    cout << endl << endl;
-    matriz.clear();
-    matriz.print();
-    cout << endl;
+    // Sentinela Coluna sendo duplicado quando é inserido um valor na mesma coluna
+    matriz.printSentinelas();
 
 }
