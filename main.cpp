e73f87db4484336ef8b6ded62b1c33d4ad2cfb65
willgner <willgnersf@alu.ufc.br>
2025-01-28 16:21:03 -0300

    lerMatriz atualizada

--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 #include <fstream>
 #include <vector>
 #include <stdexcept>
+#include <sstream>
+
 using namespace std;
 
 //Teste para ler a matriz atraves de um arquivo.
-void lerMatriz(MatrizEsparsas& m)
+void lerMatriz(MatrizEsparsas& m, string matriz)
 {
     int linha, coluna;
     double valor;
     ifstream txtFile;
-    txtFile.open("matriz.txt");
+    txtFile.open(matriz+".txt");
 
     if (txtFile.is_open())
     {
@@ -25,9 +27,30 @@ void lerMatriz(MatrizEsparsas& m)
     txtFile.close();
 }
 
+void lerMatriz(MatrizEsparsas& m)
+{
+    int linha, coluna;
+    double valor;
+    cout << "Insira o tamanho da linha e coluna, e depois as coordenadas dos valores, com o valores, e pressione enter quando terminar: " << endl;
+    cin >> linha >> coluna;
+    m.criarSentinelas(linha, coluna);
+    string data;
+    
+    cin.clear();
+    getline(cin, data);
+    stringstream buffer(data);
+    while(buffer >> linha >> coluna >> valor)
+    {
+        m.insert(linha, coluna, valor);
+    }
+}
 
 int main()
 {
-    
+    vector<MatrizEsparsas> matrizes;
+    MatrizEsparsas matriz;
+    lerMatriz(matriz);
+    matrizes.push_back(matriz);
+    matrizes.at(0).print();
 
 }
