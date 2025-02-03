8894321cd23d2fd0bb9137bb888f62fb364e9fc3
willgner <willgnersf@alu.ufc.br>
2025-02-03 19:29:17 -0300

    Menu interativo metade concluído

--- main.cpp
+++ main.cpp
@@ -61,15 +61,12 @@ void lerMatriz(MatrizEsparsas& m, string matriz)
     txtFile.close();
 }
 
-void lerMatriz(MatrizEsparsas& m)
+void lerMatriz(MatrizEsparsas& m, int linha, int coluna)
 {
-    int linha, coluna;
     double valor;
-    cout << "Insira o tamanho da linha e coluna, e depois as coordenadas dos valores, com o valores, e pressione enter quando terminar: " << endl;
-    cin >> linha >> coluna;
     m.criarSentinelas(linha, coluna);
     string data;
-    
+    cout << "Insert rows, columns and values, of all ";
     cin.clear();
     getline(cin, data);
     stringstream buffer(data);
@@ -82,20 +79,115 @@ void lerMatriz(MatrizEsparsas& m)
 int main()
 {
     vector<MatrizEsparsas> matrizes;
-    MatrizEsparsas a, b;
-    lerMatriz(a, "matriz");
-    lerMatriz(b, "matrizb");
-    matrizes.emplace_back(a);
-    matrizes.emplace_back(b);
-    cout << endl << "Matriz A: " << endl;
-    matrizes.at(0).print();
+    cout << "Welcome to matrix system!" << endl;
+    cout << "type 'help' to see the list of commands" << endl;
+    while(true){
+        string comando, opcao;
+        getline(cin, comando);
+        stringstream buffer {comando};
+        buffer >> opcao;
+        cout << "$" << buffer.str() << endl;
 
-    cout << endl << "Matriz B:" << endl;
-    matrizes.at(1).print();
-
-    cout << endl << "Soma A e B:" << endl;
-    somar(matrizes.at(0), matrizes.at(1));
-
-    cout << endl << "Multiplicacao de A e B:" << endl;
-    multiplicar(matrizes.at(0),matrizes.at(1));
+        //exit
+        if(opcao == "exit")
+        {
+            matrizes.clear();
+            break;
+        }
+        //create i j
+        else if(opcao == "create")
+        {
+            int i, j;
+            buffer >> i >> j;
+            MatrizEsparsas m;
+            m.criarSentinelas(i, j);
+            matrizes.push_back(m);
+        }
+        //createCopy i
+        else if(opcao == "createCopy")
+        {
+            int i;
+            buffer >> i;
+            try{
+            matrizes.push_back(matrizes.at(i));
+            } 
+            catch(out_of_range e)
+            {
+                cout << e.what() << endl;
+                break;
+            }
+        }
+        //read "mtrx.txt"
+        else if(opcao == "read")
+        {
+            string nome;
+            MatrizEsparsas m;
+            buffer >> nome;
+            lerMatriz(m, nome);
+            matrizes.push_back(m);
+        }
+        //update m i j value
+        else if(opcao == "update")
+        {
+            int d, i, j;
+            double x;
+            buffer >> d >> i >> j >> x;
+            matrizes.at(d).insert(i, j, x);
+        }
+        //get m i j
+        else if(opcao == "get")
+        {
+            int d, i, j;
+            buffer >> d >> i >> j;
+            cout << matrizes.at(d).get(i, j) << endl;
+        }
+        //show m
+        else if(opcao == "show")
+        {
+            int d;
+            buffer >> d;
+            matrizes.at(d).print();
+        }
+        //showidx
+        else if(opcao == "showidx")
+        {
+            for(int i = 0; i < matrizes.size(); i++)
+                cout << i << endl;
+        }
+        //sum m n
+        else if(opcao == "sum")
+        {
+            int m, n;
+            buffer >> m >> n;
+            somar(matrizes.at(m), matrizes.at(n));
+        }
+        //multiply m n
+        else if(opcao == "multiply")
+        {
+            int m, n;
+            buffer >> m >> n;
+            multiplicar(matrizes.at(m), matrizes.at(n));
+        }
+        //help
+        else if(opcao == "help")
+        {
+            cout << "HELP --------------------------------------------------------------------------------------" << endl;
+            cout << "exit............................close the program" << endl;
+            cout << "create i j......................create a new matrix with m rows and n columns" << endl;
+            cout << "createCopy i....................create copy of a matrix i of the vector of matrices" << endl;
+            cout << "read 'm.txt'....................read a sparse matrix from the file with name 'm.txt'" << endl;
+            cout << "show i..........................print the matrix i in the terminal" << endl;
+            cout << "showidx.........................show all the indexes of the matrices" << endl;
+            cout << "get m i j.......................show the value of cell(c,j) in the matrix m" << endl;
+            cout << "sum m n.........................sum the matrices m and n of the vector of matrices" << endl;
+            cout << "multiply m n....................multiply the matrices m and n of the vector of matrices" << endl;
+            cout << "update m i j value..............update the value of the cell(i,j) in the matrix m" << endl;
+            cout << "-------------------------------------------------------------------------------------------" << endl;
+        }   
+        else
+        {
+            cout << "wrong arguments" << endl;
+        }
+    }
+    return 0;
 }
